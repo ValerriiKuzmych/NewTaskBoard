1b42070f1d362d888ceb7ae6bb2c3b96e0200b2e
Valerii Kuzmych <kuzmychvalerii@gmail.com>
2024-05-22 20:31:03 +0200

    Entities created

---
+++ src/main/java/com/kuzmych/taskboard/entity/GeneralPage.java
@@ -0,0 +1,23 @@
+package com.kuzmych.taskboard.entity;
+
+public class GeneralPage {
+	
+	private long id;
+	
+	
+
+	public GeneralPage() {
+		
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/kuzmych/taskboard/entity/User.java
@@ -0,0 +1,30 @@
+package com.kuzmych.taskboard.entity;
+
+public class User {
+	
+	private long id;
+	private String name;
+	
+	
+	
+	public User() {
+		
+	}
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
